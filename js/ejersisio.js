dd4cf0d35127b2c6bb818d6c5b58ba10e696c9b6
antony3810 <111658135+antony3810@users.noreply.github.com>
2023-05-06 17:52:25 -0300

    Add files via upload

---
+++ js/ejersisio.js
@@ -0,0 +1,27 @@
+/* var nombre=prompt("porfavor ingrese su nombre");
+
+do {
+     var edad = prompt("porfabvor ingresa su edad");
+} while (isNaN(edad));
+
+if (edad>=18) {
+    alert(nombre+" es mayor de edad");
+
+} else {
+    alert(nombre+" no es mayor de edad");
+}
+ */
+/* 
+do{
+    var numero=prompt("ingresa un unmero");
+} while(isNaN(numero));
+if (numero % 2 == 0 ) {
+    alert("es par");
+} else {
+    alert("es inpar")
+}  */
+
+var listaNumeros=[prompt("ingre un numero"),
+prompt("ingre otro numero"),
+prompt("ingre el ultimo numero")];
+
